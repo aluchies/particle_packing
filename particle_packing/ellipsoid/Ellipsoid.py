dadbea1c5d7b24de20d4cb47858b51ba1b978847
Adam Luchies <adamluchies@gmail.com>
2013-12-08 19:36:00 -0600

    Ellipsoid class now uses quaternions to specify rotation.

--- particle_packing/ellipsoid/Ellipsoid.py
+++ particle_packing/ellipsoid/Ellipsoid.py
@@ -5,8 +5,8 @@ class Ellipsoid(object):
 
     """
 
-    def __init__(self, center, radii, alpha, beta, gamma):
-        """  
+    def __init__(self, center, radii, rt_ax, phi):
+        """
         """
 
         center = np.asarray(center, dtype=np.float).flatten()
@@ -23,14 +23,25 @@ class Ellipsoid(object):
         else:
             raise ValueError('incorrect input for radii input')
 
-        alpha = float(alpha)
-        self.alpha = alpha
 
-        beta = float(beta)
-        self.beta = beta
+        rt_ax = np.asarray(rt_ax, dtype=np.float).flatten()
+        if len(rt_ax) != 3:
+            raise ValueError('incorrect input for center rotation axis')
 
-        gamma = float(gamma)
-        self.gamma = gamma
+        phi = float(phi)
+        self.phi = phi
+
+        v1 = np.cos(phi / 2.) ** 2
+        v2 = (np.sin(phi / 2.) * rt_ax[0]) ** 2
+        v3 = (np.sin(phi / 2.) * rt_ax[1]) ** 2
+        v4 = (np.sin(phi / 2.) * rt_ax[2]) ** 2
+        if not np.allclose(v1 + v2 + v3 + v4, 1.0):
+            raise ValueError('incorrect input for rotation axis and phi')
+
+        self.rt_ax = rt_ax
+
+
+
 
 
 
@@ -54,7 +65,9 @@ class Ellipsoid(object):
         z_ax = np.asarray(z_ax, dtype=np.float).flatten()
 
         vol = _generate_ellipsoid_volume(x_ax, y_ax, z_ax,
-            self.center, self.radii, self.alpha, self.beta, self.gamma)
+            self.center, self.radii,
+            self.rt_ax[0], self.rt_ax[1], self.rt_ax[2],
+            self.phi)
 
         return vol
 
@@ -116,7 +129,178 @@ class Ellipsoid(object):
 
 
 
-def _generate_ellipsoid_volume(x, y, z, center, radii, alpha, beta, gamma):
+
+def _generate_ellipsoid_volume(x, y, z, center, radii, rt_x, rt_y, rt_z, phi):
+    """Generate the volume having x-, y-, and z-axes given by x, y, z. In the
+    volume, place an ellipsoid at center having given radii and to quaternion
+    rotation given by rotation axis [rt_x, rt_y, rt_z] and rotation angle phi.
+
+    The point x = [xi, yi, zi] is inside an ellipoid if 
+    (x - center)^T Q^T A Q (x - center) <= 1.
+
+    center = [xc, yc, zc] is the center point of the ellipsoid
+
+    Q = [ (1-s) * P + (s^2 - 0.5) * I]
+    s = cos( phi / 2. )
+    p = sin (phi / 2. ) * [ rt_x, rt_y, rt_z ]
+    P = p * p^T
+
+    A = [[1/a^2, 0, 0], records major and minor axis radii
+         [0, 1/b^2, 0],
+         [0, 0, 1/c^2]]
+
+    Keyword arguments:
+    x -- extent of the volume along x-axis
+    y -- extent of the volume along y-axis
+    z -- extent of the volume along z-axis
+    radii -- sphere radius
+    center -- center point of the sphere
+    rt_x --
+    rt_y --
+    rt_z --
+    phi --
+
+    Return values:
+    vol -- generated volume containing ellipse
+
+    """
+
+    # Setup matrices for quadratic form evaluation
+    rt_ax = np.asarray([rt_x, rt_y, rt_z])
+
+    B = _ellipsoid_characteristic_matrix(radii, rt_ax, phi, exponent=1.)
+
+    B = np.asarray(B)
+
+
+    # Form square position array for x and y
+    X = np.zeros((3, len(y) * len(x) * len(z)))
+    X[0] = np.tile(x - center[0], (len(z), len(y), 1)).flatten()
+    X[1] = np.tile(y - center[1], (len(z), len(x), 1)).transpose(0, 2, 1).flatten()
+    X[2] = np.tile(z - center[2], (len(y), len(x), 1)).transpose(2, 0, 1).flatten()
+
+    # Indicate points inside the ellipse
+    vol = (X * (B.dot(X))).sum(0)
+    vol = np.reshape(vol, (len(z), len(y), len(x)))
+    vol = vol <= 1.
+    vol = vol.astype(float)
+
+    return vol
+
+
+
+
+
+def _ellipsoid_characteristic_matrix(radii, rt_ax, phi, exponent=1.):
+
+    rt_ax = np.matrix(rt_ax).transpose()
+
+    A = np.matrix( np.diag(radii ** (-2 * exponent)) )
+
+    Q = _quaternion_to_rotation_matrix(rt_ax, phi)
+
+    Q = np.matrix(Q)
+
+
+
+    X = Q.T * A * Q
+
+
+
+    return X
+
+
+
+
+def _quaternion_to_rotation_matrix(rt_ax, phi):
+    """
+
+    Sources:
+    Shoemake, K., Animating rotation with quaternion curves, ACM, 19(3), 1985
+
+    """
+
+    w = np.cos(phi / 2.)
+    x = np.sin(phi / 2.) * rt_ax[0]
+    y = np.sin(phi / 2.) * rt_ax[1]
+    z = np.sin(phi / 2.) * rt_ax[2]
+
+    M = np.zeros((3,3))
+
+    M[0, 0] = 1. - 2. * (y ** 2 + z ** 2)
+    M[0, 1] = 2. * (x * y + w * z)
+    M[0, 2] = 2. * (x * z - w * y)
+
+    M[1, 0] = 2. * (x * y - w * z)
+    M[1, 1] = 1. - 2. * (x ** 2 + z ** 2)
+    M[1, 2] = 2. * (y * z + w * x)
+
+    M[2, 0] = 2. * (x * z + w * y)
+    M[2, 1] = 2. * (y * z - w * x)
+    M[2, 2] = 1. - 2. * (x ** 2 + y ** 2)
+
+
+
+    return M
+
+
+
+
+
+def rotation_matrix(alpha, beta, gamma):
+    Rx = np.array([[1., 0., 0.],
+                   [0., np.cos(alpha), -np.sin(alpha)],
+                   [0., np.sin(alpha), np.cos(alpha)]])
+    Ry = np.array([[np.cos(beta), 0., np.sin(beta)],
+                   [0., 1., 0.],
+                   [-np.sin(beta), 0., np.cos(beta)]])
+    Rz = np.array([[np.cos(gamma), -np.sin(gamma), 0.],
+                   [np.sin(gamma), np.cos(gamma), 0.],
+                   [0., 0., 1.]])
+    R = Rz.dot(Ry.dot(Rx))
+
+    return R
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def _ellipsoid_characteristic_matrix_old(radii, alpha, beta, gamma):
+    A = np.diag(1. / radii ** 2)
+    Rx = np.array([[1., 0., 0.],
+                   [0., np.cos(alpha), -np.sin(alpha)],
+                   [0., np.sin(alpha), np.cos(alpha)]])
+    Ry = np.array([[np.cos(beta), 0., np.sin(beta)],
+                   [0., 1., 0.],
+                   [-np.sin(beta), 0., np.cos(beta)]])
+    Rz = np.array([[np.cos(gamma), -np.sin(gamma), 0.],
+                   [np.sin(gamma), np.cos(gamma), 0.],
+                   [0., 0., 1.]])
+    R = Rz.dot(Ry.dot(Rx))
+    B = R.transpose().dot(A).dot(R)
+
+
+    return B
+
+
+
+
+
+
+
+
+
+
+def _generate_ellipsoid_volume_old(x, y, z, center, radii, alpha, beta, gamma):
     """Generate the volume having x-, y-, and z-axes given by x, y, z. In the
     volume, place an ellipsoid at center having given radii and according to
     alpha, beta, and gamma.
--- particle_packing/tests/test_Ellipsoid_.py
+++ particle_packing/tests/test_Ellipsoid_.py
@@ -2,6 +2,11 @@ import unittest
 import numpy as np
 from particle_packing.ellipsoid import Ellipsoid
 
+from particle_packing.ellipsoid.Ellipsoid \
+    import _quaternion_to_rotation_matrix, rotation_matrix
+
+from random import random
+
 class TestCode(unittest.TestCase):
 
 
@@ -16,13 +21,15 @@ class TestCode(unittest.TestCase):
 
         center = np.asarray([0, 0, 0], dtype=float)
         radii = np.ones(3)
-        alpha, beta, gamma = 0, 0, 0
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        rt_ax = [1., 0., 0.]
+        phi = 0.
+        c = Ellipsoid(center, radii, rt_ax, phi)
         self.assertTrue(np.allclose(c.radii, radii))
         self.assertTrue(np.allclose(c.center, center))
-        self.assertTrue(c.alpha == alpha)
-        self.assertTrue(c.beta == beta)
-        self.assertTrue(c.gamma == gamma)
+        self.assertTrue(c.rt_ax[0] == rt_ax[0])
+        self.assertTrue(c.rt_ax[1] == rt_ax[1])
+        self.assertTrue(c.rt_ax[2] == rt_ax[2])
+        self.assertTrue(c.phi == phi )
 
 
 
@@ -36,15 +43,25 @@ class TestCode(unittest.TestCase):
 
         center = 0
         radii = [1, 1, 1]
-        alpha, beta, gamma = 0, 0, 0
+        rt_ax = [1., 0., 0.]
+        phi = 0.
         self.assertRaises(ValueError, Ellipsoid, center, radii,
-            alpha, beta, gamma)
+            rt_ax, phi)
+
 
         center = [0, 0, 0]
-        radii = 'a'
-        alpha, beta, gamma = 0, 0, 0
+        rt_ax = [1., 0., 0.]
+        phi = 'a'
         self.assertRaises(ValueError, Ellipsoid, center, radii,
-            alpha, beta, gamma)
+            rt_ax, phi)
+
+
+        center = [0, 0, 0]
+        rt_ax = [1., 1., 0.]
+        phi = np.pi / 2.
+        self.assertRaises(ValueError, Ellipsoid, center, radii,
+            rt_ax, phi)
+
 
 
 
@@ -67,14 +84,13 @@ class TestCode(unittest.TestCase):
         c = 1.
         center = np.array([0., 0., 0.])
         radii = np.array([a, b, c])
-        alpha = 0.
-        beta = 0.
-        gamma = 0.
+        rt_ax = [1., 0., 0.]
+        phi = 0.
         x = np.linspace(-1., 1., 3)
         y = np.linspace(-1., 1., 3)
         z = np.linspace(-1., 1., 3)
 
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        c = Ellipsoid(center, radii, rt_ax, phi)
         vol = c.generate_volume(x, y, z)
 
         arr = np.array([[[0, 0, 0],
@@ -104,14 +120,13 @@ class TestCode(unittest.TestCase):
         c = 0.5
         center = np.array([0., 0., 0.])
         radii = np.array([a, b, c])
-        alpha = 0.
-        beta = 0.
-        gamma = 0.
+        rt_ax = [1., 0., 0.]
+        phi = 0.
         x = np.linspace(-1., 1., 3)
         y = np.linspace(-1., 1., 3)
         z = np.linspace(-1., 1., 3)
 
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        c = Ellipsoid(center, radii, rt_ax, phi)
         vol = c.generate_volume(x, y, z)
 
         arr = np.array([[[0, 0, 0],
@@ -141,14 +156,13 @@ class TestCode(unittest.TestCase):
         c = 1.
         center = np.array([0., 0., 1.])
         radii = np.array([a, b, c])
-        alpha = 0.
-        beta = 0.
-        gamma = 0.
+        rt_ax = [1., 0., 0.]
+        phi = 0.
         x = np.linspace(-1., 1., 3)
         y = np.linspace(-1., 1., 3)
         z = np.linspace(-1., 1., 3)
 
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        c = Ellipsoid(center, radii, rt_ax, phi)
         vol = c.generate_volume(x, y, z)
 
         arr = np.array([[[0, 0, 0],
@@ -177,14 +191,13 @@ class TestCode(unittest.TestCase):
         c = 1.
         center = np.array([0., 1., 0.])
         radii = np.array([a, b, c])
-        alpha = 0.
-        beta = 0.
-        gamma = 0.
+        rt_ax = [1., 0., 0.]
+        phi = 0.
         x = np.linspace(-1., 1., 3)
         y = np.linspace(-1., 1., 3)
         z = np.linspace(-1., 1., 3)
 
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        c = Ellipsoid(center, radii, rt_ax, phi)
         vol = c.generate_volume(x, y, z)
 
         arr = np.array([[[0, 0, 0],
@@ -204,40 +217,92 @@ class TestCode(unittest.TestCase):
     def test5_generate_volume(self):
         """
 
-        Rotated ellipsoid
+        Rotate ellipsoid
 
         """
 
         a = 0.5
         b = np.sqrt(2)
-        c = 1
+        c = 1.
         center = np.array([0., 0., 0.])
         radii = np.array([a, b, c])
-        alpha = 0.
-        beta = 0.
-        gamma = np.pi / 4.
+        rt_ax = [0., 0., 1.]
+        phi = np.pi / 4.
         x = np.linspace(-1., 1., 3)
         y = np.linspace(-1., 1., 3)
         z = np.linspace(-1., 1., 3)
 
-        c = Ellipsoid(center, radii, alpha, beta, gamma)
+        c = Ellipsoid(center, radii, rt_ax, phi)
         vol = c.generate_volume(x, y, z)
 
         arr = np.array([[[0, 0, 0],
                          [0, 1, 0],
                          [0, 0, 0]],
 
-                        [[1, 0, 0],
+                        [[0, 0, 1],
                          [0, 1, 0],
-                         [0, 0, 1]],
+                         [1, 0, 0]],
 
                         [[0, 0, 0],
                          [0, 1, 0],
                          [0, 0, 0]]])
 
 
-
         self.assertTrue(np.allclose(arr, vol))
+
+
+
+
+
+
+    def test1__quaternion_to_rotation_matrix(self):
+        """
+
+        Test simple axial rotations
+
+        """
+
+
+        # x-axis
+        for i in xrange(100):
+            phi = np.pi * random()
+            rot_ax = np.asarray([1., 0., 0.])
+            R1 = _quaternion_to_rotation_matrix(rot_ax, phi)
+
+            alpha, beta, gamma, = -phi, 0., 0.
+            R2 = rotation_matrix(alpha, beta, gamma)
+
+            self.assertTrue(np.allclose(R1, R2))
+
+
+        # y-axis
+        for i in xrange(100):
+            phi = np.pi * random()
+            rot_ax = np.asarray([0., 1., 0.])
+            R1 = _quaternion_to_rotation_matrix(rot_ax, phi)
+
+            alpha, beta, gamma, = 0., -phi, 0.
+            R2 = rotation_matrix(alpha, beta, gamma)
+
+            self.assertTrue(np.allclose(R1, R2))
+
+
+        # z-axis
+        for i in xrange(100):
+            phi = np.pi * random()
+            rot_ax = np.asarray([0., 0., 1.])
+            R1 = _quaternion_to_rotation_matrix(rot_ax, phi)
+
+            alpha, beta, gamma, = 0., 0., -phi
+            R2 = rotation_matrix(alpha, beta, gamma)
+
+            self.assertTrue(np.allclose(R1, R2))
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
