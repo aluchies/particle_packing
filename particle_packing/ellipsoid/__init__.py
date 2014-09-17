68f94e17e102ed562277800dde8f1c8d6b882b83
Adam Luchies <adamluchies@gmail.com>
2014-09-17 14:20:03 -0500

    Make quaternion_to_rotation_matrix availabe in particle_packing.ellipse.

--- particle_packing/ellipsoid/__init__.py
+++ particle_packing/ellipsoid/__init__.py
@@ -1,5 +1,6 @@
 from ..ext.ellipsoid import overlap_potential, cube_container_potential
 
 from Ellipsoid import Ellipsoid, overlap_potential_py, cube_container_potential_py
+from Ellipsoid import _quaternion_to_rotation_matrix as quaternion_to_rotation_matrix
 
 import pack
