c7463dc3a7523b15f69315e9c0ffe09bd989ceeb
Adam Luchies <adamluchies@gmail.com>
2015-07-09 12:00:59 -0400

    Deleted one of the tests for Spherical_Gaussian

--- particle_packing/tests/test_Spherical_Gaussian.py
+++ particle_packing/tests/test_Spherical_Gaussian.py
@@ -34,37 +34,9 @@ class TestCode(unittest.TestCase):
 
         center = np.asarray([0,0,0])
         radius = 'a'
-        self.assertRaises(ValueError, Spherical_Gaussian, center, radius)
+        self.assertRaises(ValueError, Spherical_Gaussian, center, radius_eff)
 
 
 
 
 
-
-    def test1_generate_volume_spherical_gaussian(self):
-        """
-
-        Test generate_volume_spherical_gaussian() method for Sphere class
-
-        """
-
-        radius = 0.2
-        center = 0.5 * np.ones(3)
-
-
-        x_ax = np.linspace(0, 1, 3)
-        y_ax = np.linspace(0, 1, 3)
-        z_ax = np.linspace(0, 1, 3)
-
-        c = Spherical_Gaussian(center, radius)
-        subvol = c.generate_volume(x_ax, y_ax, z_ax)
-
-        arr = np.zeros((3,3,3))
-        arr[0,1,1] = 1
-        arr[1,1,1] = 1
-        arr[1,1,0] = 1
-        arr[1,0,1] = 1
-        arr[1,1,2] = 1
-        arr[1,2,1] = 1
-        arr[2,1,1] = 1
-        self.assertTrue(np.allclose(arr, subvol))
